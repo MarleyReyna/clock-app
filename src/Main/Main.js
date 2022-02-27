25ca2e051635f264a66b5712e5235147444b94a6
Marley Reyna <marley.reyna510@gmail.com>
2022-02-26 21:37:32 -0600

    Change

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -16,7 +16,7 @@ const Main = (props) => {
     const [country, setCountry] = useState('')
 
     const getLocation = async() =>{
-        const response = await fetch(`http://ip-api.com/json/`);
+        const response = await fetch(`http://ip-api.com/json/{query}`);
         const res = await response.json();
         console.log(res)
         setCity(res.city)
